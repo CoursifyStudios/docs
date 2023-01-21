bad5c4bbaa5cf0c8956d6d3a231b5f5395eccb7e
Brandon H <brandon@coursify.studio>
2023-01-21 00:21:07 -0800

    fixed grammer errors

--- theme.config.tsx
+++ theme.config.tsx
@@ -15,16 +15,15 @@ const config: DocsThemeConfig = {
         fill = "currentColor"
         d="M21,1l6.973,2c0,0-0.95,0.754-0.973,3s0,6,0,6h14l4,4l-2,1c0,0,0.338,19.454-1,23c-1.03,2.729-4.061,5.013-7,5c-4.833-0.021-6-3-6-3l2-5c0,0,0.97,1.998,3,2c1.921,0.002,2.956-1.132,3-9.5c0.045-8.723,0-11.5,0-11.5H27c0,0,0.551,16.428-16,28c-1.889-2.317-4-5-4-5s12.997-7.139,14-23c-7.061-0.095-12.043,0.15-14,1l-2-7h16V1z" />
       </svg>
-      <span style={{ marginLeft: ".4em", fontWeight: 600 }}>Karasu</span>
+      <span style={{ marginLeft: ".4em", fontWeight: 600, fontSize: "22px" }}>Karasu</span>
     </>
   ),
   head: () => {
     const { asPath } = useRouter()
     const { frontMatter } = useConfig()
     return <>
-      <meta property="og:title" content="Karasu"/>
+      <title>Karasu Docs</title>
       <meta property="og:url" content={`https://my-app.com${asPath}`} />
-      <meta property="og:title" content={frontMatter.title || 'Karasu'} />
       <meta property="og:description" content={frontMatter.description || 'A next generation LMS'} />
     </>
   },
